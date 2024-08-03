3191b1b8c2b3f40e1df7e2673a3bd53a5d371e19
just_xolotl <157688992+xolotlwastaken@users.noreply.github.com>
2024-08-03 20:26:51 +0800

    Update signUpWithEmail.dart

--- signUpWithEmail.dart
+++ signUpWithEmail.dart
@@ -8,6 +8,8 @@ import 'package:flutter/material.dart';
 // Begin custom action code
 // DO NOT REMOVE OR MODIFY THE CODE ABOVE!
 
+import 'package:supabase_flutter/supabase_flutter.dart';
+
 Future<String?> signUpWithEmail(
   String email,
   String password,
